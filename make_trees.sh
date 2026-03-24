9adb5d19914211ab0d3d6c119d3dc9a81b001f36
swik43 <Sam.wikander@gmail.com>
2026-03-24 12:31:41 +0100

    updated contents_config for hrw

--- make_trees.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-
-tree /Users/samwikander/Documents/Thesis/Reports > ../tree__Reports.txt
-tree /Users/samwikander/Documents/Thesis/WR_split_pdf > ../tree_WR_split_pdf.txt
-tree /Users/samwikander/Documents/Thesis/WR_split_markdown > ../tree_WR_split_markdown.txt
-tree "/Users/samwikander/Documents/Thesis/U.S State Reports MD" > ../tree__US_State_Reports.txt 
